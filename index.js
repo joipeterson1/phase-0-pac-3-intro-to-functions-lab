8f09c77af87ef3c3da26b80696b3adab20cad5b1
Dalecia <petersondalecia@gmail.com>
2023-10-09 16:39:40 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string){
+    //todo
+}
+function shout(string) {
+    return string;
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string){
+    //todo
+  }
+  function whisper(string){
+    return string;
+  }
+  function whisper (string){
+    return string.toLowerCase()
+  }
+  function logShout(string){
+    console.log(logShout.UpperCase())
+  }
+function logShout(string){
+    //to define logShout
+}
+function logShout(string){
+    console.log(`HELLO`)
+}
+function logWhisper(string){
+    //to define logShout
+}
+function logWhisper(string){
+    console.log(`hello`)
+}
+function sayHiToHeadphonedRoommate(string){
+    //defined string
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string === 'hello')
+     return"I can\'t hear you!"
+        else if (string === 'HELLO')
+      return "YES INDEED!"
+       else (string === "Let\'s have dinner together!")
+       return "I would love to!"
+  }
